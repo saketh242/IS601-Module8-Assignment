4630abf3a4ca3c90c76ac2527f57cbfe5fa36fdf
Saketh Puramsetti <saketh.puramsetti@outlook.com>
2025-10-26 22:48:49 -0400

    fix tests

--- tests/e2e/test_e2e.py
+++ tests/e2e/test_e2e.py
@@ -58,7 +58,7 @@ def test_calculator_divide_by_zero():
             page.fill('#a', '10')
             page.fill('#b', '0')
             page.click('button:text("Divide")')
-            assert page.inner_text('#result') == 'Error: Cannot divide by zero!'
+            assert page.inner_text('#result') == 'Error: Cannot divide by zero💀'
             browser.close()
     finally:
         process.terminate()
