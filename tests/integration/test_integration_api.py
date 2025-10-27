29927cfdf17e31c30a8e3974acae71ab8870c7fb
Saketh Puramsetti <saketh.puramsetti@outlook.com>
2025-10-26 22:41:18 -0400

    fix tests

--- tests/integration/test_integration_api.py
+++ tests/integration/test_integration_api.py
@@ -1,10 +1,12 @@
 
+import os
 import sys
 import pytest  
 from fastapi.testclient import TestClient 
 from main import app  
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
+
 
 @pytest.fixture
 def client():
